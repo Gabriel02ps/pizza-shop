0d9defead20ef4c2756721a508345222323b81ee
Gabriel02ps <gabriel02ps@gmail.com>
2024-03-18 11:06:21 -0300

    chore: add order status functionality

---
+++ src/api/approve-order.ts
@@ -0,0 +1,9 @@
+import { api } from '@/lib/axios'
+
+export interface AprroveOrderParams {
+  orderId: string
+}
+
+export async function aprroveOrder({ orderId }: AprroveOrderParams) {
+  await api.patch(`/orders/${orderId}/approve`)
+}
